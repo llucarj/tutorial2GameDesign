ea8c65809a9fd718742a2fa702b90f6d829c51ca
Lucas Araújo <61202832+llucarj@users.noreply.github.com>
2021-09-22 22:04:12 -0400

    upload: Tutorial 2

---
+++ Tutorial 2/Assets/Pixel Adventure 1/Assets/Scripts/GameControler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class GameControler : MonoBehaviour
+{
+
+    public int totalScore;
+    public Text scoreText;
+
+    public GameObject gameOver;
+
+    public static GameControler instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+    
+    public void updateScoreText()
+    {
+        scoreText.text = totalScore.ToString();
+    }
+
+    public void ShowGameOver()
+    {
+        gameOver.SetActive(true);
+    }
+
+    public void RestartGame(string lvlName)
+    {
+        SceneManager.LoadScene(lvlName);
+    }
+}
